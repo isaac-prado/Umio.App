97be405693f22b785bf196304cd2af21b21d8f38
Isaac Prado <isaac.prado@tech4h.com.br>
2025-06-04 19:26:57 -0300

    REBASED: TOAST no login

--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -19,7 +19,6 @@ export default function LoginScreen() {
     try {
       await loginUser(email, senha);
     } catch (error: any) {
-      console.log(error);
       Toast.show({
         type: "error",
         text1: "Login Incorreto!",
